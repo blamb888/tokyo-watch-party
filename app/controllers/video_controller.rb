442f047228122ae42861b25a0b7ca5da89c6f2a5
Brandon Lamb <blamb888@gmail.com>
2021-03-31 21:34:11 +0900

    create Video controller

---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,16 @@
+class VideoController < ApplicationController
+  def landing
+  end
+
+  def index
+  end
+
+  def screenshare
+  end
+
+  def name
+  end
+
+  def chat
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'video/landing'
+  # get 'video/index'
+  # get 'video/screenshare'
+  # get 'video/name'
+  # get 'video/chat'
+  # Generated routes ^
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'video#landing'
   get '/party', to: 'video#index'
