c896c76da6b89bff4ba192912a6109efb143b036
Brandon Lamb <blamb888@gmail.com>
2021-03-31 21:29:08 +0900

    add null/default: false to Session model migration file

--- db/migrate/20210331122721_create_sessions.rb
+++ db/migrate/20210331122721_create_sessions.rb
@@ -1,8 +1,8 @@
 class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions do |t|
-      t.string :session_id
-      t.boolean :expired
+      t.string :session_id, null: false
+      t.boolean :expired, default: false
 
       t.timestamps
     end
