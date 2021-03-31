0c4a5e42f11ffa7f6c9486d2af25332af5f21e39
Brandon Lamb <blamb888@gmail.com>
2021-03-31 21:41:04 +0900

    add create_or_load_session_id to Session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,17 @@
 class Session < ApplicationRecord
+  def self.create_or_load_session_id
+    if Session.any?
+      last_session = Session.last
+      if last_session && last_session.expired == false
+        @session_id = last_session.session_id
+        @session_id
+      elsif (last_session && last_session.expired == true) || !last_session
+        @session_id = create_new_session
+      else
+        raise 'Something went wrong with the session creation!'
+      end
+    else
+      @session_id = create_new_session
+    end
+  end
 end
