08fd4d3f5b4efb9950e9eedb35cc4419bba1d340
Brandon Lamb <blamb888@gmail.com>
2021-03-31 21:31:12 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'video#landing'
+  get '/party', to: 'video#index'
+  get '/screenshare', to: 'video#screenshare'
+  post '/name', to: 'video#name'
+  post '/chat/send', to: 'video#chat'
 end
