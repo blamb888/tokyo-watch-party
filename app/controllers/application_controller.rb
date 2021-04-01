81204da091de48649496eed0fd740889e51efd6e
Brandon Lamb <blamb888@gmail.com>
2021-04-01 13:16:00 +0900

    add before_actions to application controller. open site_config.yml file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_site_lang_options
+  before_action :set_site_welcome_options
+  before_action :set_welcome_form_options
+  before_action :set_site_navbar_options
+  before_action :set_site_chat_options
+  CONFIG = YAML.load_file("#{Rails.root}/config/site_info.yml")
 end
