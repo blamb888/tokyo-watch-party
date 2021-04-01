6a8155161d0d4a0dbd5a4826600076b94e36d47e
Brandon Lamb <blamb888@gmail.com>
2021-04-01 13:02:12 +0900

    add opentok vars to video_controller

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,4 +1,13 @@
 class VideoController < ApplicationController
+  def set_opentok_vars
+    @api_key = ENV['OPENTOK_API_KEY']
+    @api_secret = ENV['OPENTOK_API_SECRET']
+    @session_id = Session.create_or_load_session_id
+    @moderator_name = ENV['MODERATOR_NAME']
+    @name ||= params[:name]
+    @token = Session.create_token(@name, @moderator_name, @session_id)
+  end
+
   def landing
   end
 
