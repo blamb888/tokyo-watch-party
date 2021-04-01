79f36940a1070a527ab477f157c59b8899650aa9
Brandon Lamb <blamb888@gmail.com>
2021-04-01 16:55:02 +0900

    add subscribe method to screenshare.js

--- app/javascript/packs/screenshare.js
+++ app/javascript/packs/screenshare.js
@@ -37,6 +37,77 @@ export default class Screenshare {
     });
   }
 
+  setupAudioPublisher() {
+    var self = this;
+    var audioPublishOptions = {};
+    audioPublishOptions.insertMode = 'append';
+    audioPublishOptions.publishVideo = false;
+    var audio_publisher = OT.initPublisher('audio', audioPublishOptions,
+      function (error) {
+        if (error) {
+          console.log(error);
+        } else {
+          self.session.publish(audio_publisher, function (error) {
+            if (error) {
+              console.log(error);
+            }
+          });
+        };
+      }
+    );
+  }
 
+  setupPublisher() {
+    var self = this;
+    var publishOptions = {};
+    publishOptions.videoSource = 'screen';
+    publishOptions.insertMode = 'append';
+    publishOptions.height = '100%';
+    publishOptions.width = '100%';
+    var screen_publisher = OT.initPublisher('screenshare', publishOptions,
+      function (error) {
+        if (error) {
+          console.log(error);
+        } else {
+          self.session.publish(screen_publisher, function (error) {
+            if (error) {
+              console.log(error);
+            };
+          });
+        };
+      }
+    );
+  }
+
+  subscribe() {
+    var self = this;
+    this.watchLink.style.display = "none";
+    this.session.on({
+      streamCreated: function(event) {
+        console.log(event);
+        if (event.stream.hasVideo == true) {
+          self.session.subscribe(event.stream, 'screenshare,' {
+            insertMode: 'append',
+            width: "100%",
+            height: "100%"
+          }, function(error) {
+            if (error) {
+              console.error('Failed to subscribe to video feed', error);
+            }
+          });
+        } else if (event.stream.hasVideo == false) {
+          self.session.subscribe(event.stream, 'audio', {
+            insertMode: 'append',
+            width: "0px",
+            height: "0px"
+          }, function(error) {
+            if (error) {
+              console.error('Failed to subscribe to audio feed', error);
+            }
+          });
+        };
+      }
+    });
+  }
 
 }
